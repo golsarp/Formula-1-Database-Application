d0706a193071b6c52b0ab4b7bd8b1c300afa30cb
golsarp <127028409+golsarp@users.noreply.github.com>
2023-03-05 19:19:17 +0300

    Delete f1_project directory

--- f1_project/src/res_const.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'dbConnect.php';
-require_once 'functions.php';
-session_start();
-
-
-list($result1, $result2) = const_results($conn,$_GET['link']);
-print_table_const($result1, $result2);
